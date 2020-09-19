8ccade1dbdd84e9c2da17a0c10a5c27302c79c92
George Dialynas <dialynas1@yahoo.com>
2020-09-19 12:09:21 +0300

    implement map based services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.dialynas.sfgpetclinic.services.map;
+
+import com.dialynas.sfgpetclinic.model.Vet;
+import com.dialynas.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(),vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
