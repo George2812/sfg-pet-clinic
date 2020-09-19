7c111e838bb87952e692b3045c9f9c5184ba2546
George Dialynas <dialynas1@yahoo.com>
2020-09-19 08:39:49 +0300

    implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package com.dialynas.sfgpetclinic.services;
+
+import com.dialynas.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
