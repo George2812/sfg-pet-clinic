a5a5c9d39d50efa1cf8138c4a23e233cf375aa90
George Dialynas <dialynas1@yahoo.com>
2020-09-29 17:51:32 +0300

    CRUD Tests for Owner Map Service. Closes #50.

--- pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/model/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.dialynas.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
@@ -11,11 +13,15 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
 
 }
