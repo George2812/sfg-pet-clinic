d11a796218ec66eced0123b487f5c504a0e4293a
George Dialynas <dialynas1@yahoo.com>
2020-10-04 11:54:12 +0300

    implement find owner.#closes 55.

--- pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dialynas/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,10 @@ package com.dialynas.sfgpetclinic.repositories;
 import com.dialynas.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
